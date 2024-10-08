d2859ce25c53f3963e9e00db51dc510426863a36
maralienet <maralienet@gmail.com>
2024-10-08 19:24:48 +0300

    git commit

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import "./style.sass";
 import { Sections, SectionsCollapsible } from "../Components/Sections";
 import Wishes from "../Components/Wishes";
 
