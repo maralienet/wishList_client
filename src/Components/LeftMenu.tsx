a8918cee1c59b52c022b4bac6852ec03d6a76ebc
maralienet <maralienet@gmail.com>
2024-10-08 22:36:23 +0300

    git commit

--- src/Components/LeftMenu.tsx
+++ src/Components/LeftMenu.tsx
@@ -1,28 +1,27 @@
 import React, { useState } from "react";
-import logo from '../pics/logo.png';
+import logo from "../pics/logo.png";
 
 const LeftMenu: React.FC = () => {
+  function handleClick(e: Event) {
+    if(e)
+        console.log()
+  }
+
   return (
     <aside className="leftMenu">
-        <div className="top">
-            <img src={logo} className="logo" />
-            <p>Вишлистик</p>
-        </div>
-        <div className="search">
-            <label htmlFor="search">Ввести код друга</label>
-            <input type="number" placeholder="Код друга" id='search'></input>
-        </div>
-        <div className="menu">
-            <div className="menuItem">
-                Вход
-            </div>
-            <div className="menuItem">
-                Регистрация
-            </div>
-            <div className="menuItem">
-                О проекте
-            </div>
-        </div>
+      <div className="top">
+        <img src={logo} className="logo" />
+        <p>Вишлистик</p>
+      </div>
+      <div className="search">
+        <label htmlFor="search">Ввести код друга</label>
+        <input type="search" placeholder="Код друга" id="search"></input>
+      </div>
+      <div className="menu">
+        <div className="menuItem">Вход</div>
+        <div className="menuItem">Регистрация</div>
+        <div className="menuItem">О проекте</div>
+      </div>
     </aside>
   );
 };
