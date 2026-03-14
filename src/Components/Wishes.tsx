55285f11f74793b4608d36a3cdb0989f5cef362e
maralienet <maralienet@gmail.com>
2026-03-14 17:09:53 +0300

    -git commit

--- src/Components/Wishes.tsx
+++ src/Components/Wishes.tsx
@@ -71,7 +71,7 @@ const Wishes: React.FC<WishesProps> = ({ section }) => {
     <div className="wishes">
       <p
         className="secName"
-        onClick={() => console.log({ sect })}
+        onClick={() => console.log({ section })}
       >
         --- {section ? `${sect}` : "Выбери категорию"} ---
       </p>
